00eae5e8f661c8142bcd3fa0bcb5c2fc019ccfbd
faransaeed41-sys <faransaeed41@gmail.com>
2026-02-26 10:14:46 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Faran App')),
+        body: Center(child: Text('App is Working!')),
+      ),
+    ));
